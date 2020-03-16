e744525767bba28c7a9fbe1d3c30e426e9e848cb
dev-sm <saimain.dev@gmail.com>
2020-03-16 14:16:47 +0630

    API Song Collection Modified

--- app/Http/Resources/Song/SongCollection.php
+++ app/Http/Resources/Song/SongCollection.php
@@ -21,6 +21,20 @@ class SongCollection extends JsonResource
             'artist' => $this->artist->name,
             'category' => $this->category->name,
             'album' => $this->album->name,
+            'artist' => [
+                'artist_name' => $this->artist->name,
+                'artist_detail' => route('artist.show', $this->artist->id),
+            ],
+            'category' => [
+                'category_name' => $this->category->name,
+                'category_detail' => route('category.show', $this->category->id),
+            ],
+            'album' => [
+                'album_name' => $this->album->name,
+                'album_detail' => route('album.show', $this->album->id),
+            ],
+            'lyric' => $this->lyric,
+            'source' => $this->source,
             'detail' => route('song.show', $this->id)
         ];
     }
--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,10 @@ Route::apiResource('/artist', 'Api\ArtistController');
 Route::apiResource('/category', 'Api\CategoryController');
 
 Route::apiResource('/album', 'Api\AlbumController');
+
+
+
+Route::post('login', 'Api\UserController@login');
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::post('details', 'API\UserController@details');
+});
